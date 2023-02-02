9fdd5336c9e230b39116163a8f6f8e50d73ab564
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2023-02-02 19:24:25 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,14 +5,56 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const testObj = { a: 3, b: 6, c: 9 };
+const getRandomLetter = () => {
+  const min = 65
+  const max = 90
+  const result = Math.floor(Math.random() * (max - min + 1) + min)
+  return String.fromCharCode(result)
+}
+const getRandomDigit = () => {
+  const min = 0
+  const max = 9
+  const result = Math.floor(Math.random() * (max - min + 1) + min)
+  return result
+}
+const robotsNamesBase = new Set()
 
-const doubleValues = (obj) => {
-  const returnObj = Object.create(obj);
-  for (let key in returnObj) {
-    returnObj[key] = returnObj[key] * 2;
+export class Robot {
+  #name
+  constructor() {
+    this.#name = this.asignRobotName()
   }
-  return returnObj;
-};
+  makeRobotName() {
+    return (
+      getRandomLetter() +
+      getRandomLetter() +
+      getRandomDigit() +
+      getRandomDigit() +
+      getRandomDigit()
+    )
+  }
+  asignRobotName() {
+    while (1) {
+      const newName = this.makeRobotName()
+      if (!robotsNamesBase.has(newName)) {
+        robotsNamesBase.add(newName)
+        return newName
+      }
+    }
+  }
 
-console.log(doubleValues(testObj));
+  get name() {
+    return this.#name
+  }
+
+  reset() {
+    const { name: oldName } = this
+    this.#name = this.asignRobotName()
+    robotsNamesBase.delete(oldName)
+  }
+}
+
+const robot = new Robot()
+console.log(robot.name)
+robot.reset()
+console.log(robot.name)
